21082414e80921220c82c1a2ddbfd36f7ecef6b2
Bismarrck <Bismarrck@me.com>
2014-04-24 20:01:54 +0800

    Add PRConjugateGradientDescent to this package.

--- pybrain/auxiliary/__init__.py
+++ pybrain/auxiliary/__init__.py
@@ -1,3 +1,4 @@
 from pybrain.auxiliary.gradientdescent import GradientDescent
+from pybrain.auxiliary.gradientdescent import PRConjugateGradientDescent
 from pybrain.auxiliary.gaussprocess import GaussianProcess
 from pybrain.auxiliary.importancemixing import importanceMixing
